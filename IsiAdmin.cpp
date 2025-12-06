7efd9d1385438a813fbde3a402001e69d09aebdd
armand-ctrl <armandsofian12@gmail.com>
2025-12-06 23:15:55 +0700

    Refactor admin functions for member and stock management

--- IsiAdmin.cpp
+++ IsiAdmin.cpp
@@ -10,7 +10,7 @@ void admin::lihatMember(const std::map<std::string, Member>& daftarMember) {
     std::cout << "Daftar Member:\n";
     for (const auto& pair : daftarMember) {
         const Member& member = pair.second;
-        std::cout << "Nama: " << member.getnama() << ", Umur :" << member.umur << ", Kelamin: " << member.kelamin << ", Roti Dibeli: " << member.getRotiDibeli() << std::endl;
+        std ::cout << "Nama: " << member.getnama() << ", Username: " << member.getUsername() << ", Umur: " << member.umur << ", Kelamin: " << member.kelamin << ", Roti Dibeli: " << member.getRotiDibeli() << std::endl;
     }
 }
 
@@ -28,8 +28,7 @@ void admin::keuntungan(const std::vector<Roti>& daftarRoti) {
 }
 
 void admin::hapusMember(const std::string& username) {
-// fungsi untuk menghapus member, untuk admin
-    lihatMember(daftarMember); 
+// fungsi untuk menghapus member, untuk admin 
     auto it = daftarMember.find(username);
     if (it != daftarMember.end()) {
         daftarMember.erase(it);
@@ -106,3 +105,4 @@ void admin::kurangistok(const std::string& namaRoti, int jumlah, std::vector<Rot
     }
     std::cout << "Roti dengan nama tersebut tidak ditemukan.\n";
 }
+
